48bc51efe69b5a63d5b104e28f1fc345bd5a5680
Guilherme Aguiar <kadeguilherme@gmail.com>
2020-07-10 14:57:28 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vamos cozinhar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Vamos conzinhar?',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'vamos cozinhar',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
